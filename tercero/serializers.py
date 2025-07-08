137885af4bcb82c9c5972dfe26f7f1ceb94d4783
Edisonnarvaez <edison.narvaez@campusucc.edu.co>
2025-07-08 08:58:43 -0500

    ajustes base2

--- tercero/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Tercero
-
-class TerceroSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tercero
-        fields = '__all__'
