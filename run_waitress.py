b9fcf047aa6d2adfb1cf0ff6f197cbe166efdf1e
RedMedicronIPS <profesionaltic@redmedicronips.com.co>
2025-08-05 14:29:54 +0200

    ajustes produccion y readme

--- run_waitress.py
+++ run_waitress.py
@@ -1,5 +1,5 @@
 from waitress import serve
-from backend.wsgi import application  # Reemplaza 'backend' por el nombre correcto si es otro
+from backend.wsgi import application  
 
 serve(application, host="127.0.0.1", port=8081)
 
